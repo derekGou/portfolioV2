a417365e8518b9ae6523e966269541c2881f3edd
Derek Gou <134746288+derekGou@users.noreply.github.com>
2024-04-05 19:08:06 -0400

    Add files via upload

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -1,11 +1,10 @@
 window.addEventListener("resize", function(){
     document.getElementsByTagName("loader")[0].style.display = "flex";
     resizeCanvas();
-    resizeMyName();
+    resizeMyName2();
     setTimeout(function(){
         document.getElementsByTagName("loader")[0].style.display = "none";
     }, 1000);
-    console.log('haha')
 });
 window.addEventListener("scroll", function(){
     scrolling();
