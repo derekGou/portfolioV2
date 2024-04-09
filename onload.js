ddcc68a9e8c8ac4c0570025cb6e1a44887748d68
Derek Gou <134746288+derekGou@users.noreply.github.com>
2024-04-09 15:22:44 -0400

    Delete onload.js

--- onload.js
+++
@@ -1,8 +0,0 @@
-pixelsize();
-$('#name').hide();
-setTimeout(function(){
-    document.getElementsByTagName("loader")[0].style.display = "none";
-}, 1000);
-setTimeout(function(){
-    $('#name').fadeIn(2000);
-}, 1000);
